b8a14df3a1b127151809943282da3e20d8809766
Khadija Khadija <khadija@example.com>
2026-07-05 23:57:30 +0100

    AnimeCharacters pages UI

--- src/pages/AnimeCharacters.jsx
+++ src/pages/AnimeCharacters.jsx
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import { getAnimeCharacters } from "../api/jikan";
 import { Link } from "react-router-dom";
 import Footer from "../components/common/Footer.jsx";
+import  "../style/AnimeCharacters.css"
 
 
 export default function AnimeCharacters() {
@@ -21,23 +22,30 @@ export default function AnimeCharacters() {
     fetchDataById(id);
   }, [id]);
 
-  return (
-    <>
+ return (
+  <>
+    <div className="characters-grid">
+
       {characters.map((character) => (
-        <Link key={character.character.mal_id} to={`/characters/${character.character.mal_id}`}>
+        <Link
+          key={character.character.mal_id}
+          to={`/characters/${character.character.mal_id}`}
+          className="character-card"
+        >
           <div>
-          <img
-            src={character?.character?.images?.jpg?.image_url}
-            alt={character?.character?.name}
-          />
-          <p>{character.character.name}</p>
-          <p>{character.character.role}</p>
+            <img
+              src={character?.character?.images?.jpg?.image_url}
+              alt={character?.character?.name}
+            />
+            <p className="name">{character.character.name}</p>
+            <p className="role">{character.character.role}</p>
           </div>
         </Link>
       ))}
-      <div>
-        <Footer/>
-      </div>
-    </>
-  );
+
+    </div>
+
+    <Footer />
+  </>
+);
 }
